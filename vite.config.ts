a012e3ed93813a5c544c79598166d6a86129b88e
Red Di <reddi@Reds-Mac-mini.local>
2026-05-02 00:15:02 +0800

    chore: add png file

--- vite.config.ts
+++ vite.config.ts
@@ -14,12 +14,12 @@ export default defineConfig({
         theme_color: '#0f172a',
         icons: [
           {
-            src: '/icons/icon-192.png',
+            src: '/expense.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-512.png',
+            src: '/expense.png',
             sizes: '512x512',
             type: 'image/png'
           }
